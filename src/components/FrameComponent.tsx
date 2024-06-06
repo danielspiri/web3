052d452bf0cb3bc7d778c53403fa6959bc698095
Daniel Spiri <157709420+danielspiri@users.noreply.github.com>
2024-06-06 15:54:06 +0200

    Update FrameComponent.tsx

--- src/components/FrameComponent.tsx
+++ src/components/FrameComponent.tsx
@@ -25,7 +25,7 @@ const FrameComponent = forwardRef<HTMLFormElement, FrameComponentType>(({
     });
 
     try {
-      const response = await fetch(`https://web3-eight-mocha.vercel.app/backend/subscribe`, {  // URL de tu servidor Flask
+      const response = await fetch(`https://backend-omega-steel.vercel.app/api/subscribe`, {  // URL de tu servidor Flask
         method: 'POST',
         headers: headers,
         body: data
