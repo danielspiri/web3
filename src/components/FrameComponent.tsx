f75968be3f3a58cfdc6badf6ef5b028384684e2b
Daniel Spiri <157709420+danielspiri@users.noreply.github.com>
2024-06-06 15:20:28 +0200

    Update FrameComponent.tsx

--- src/components/FrameComponent.tsx
+++ src/components/FrameComponent.tsx
@@ -25,7 +25,7 @@ const FrameComponent = forwardRef<HTMLFormElement, FrameComponentType>(({
     });
 
     try {
-      const response = await fetch(`http://localhost:5000/subscribe`, {  // URL de tu servidor Flask
+      const response = await fetch(`https://web3-2ajt.vercel.app/api/subscribe`, {  // URL de tu servidor Flask
         method: 'POST',
         headers: headers,
         body: data
