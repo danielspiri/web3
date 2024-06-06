e3fc8d62b76ada7353c0d0506512d46ee7b4519f
Daniel Spiri <157709420+danielspiri@users.noreply.github.com>
2024-06-06 15:26:55 +0200

    Update subscribe.py

--- backend/api/subscribe.py
+++ backend/api/subscribe.py
@@ -11,7 +11,7 @@ def subscribe():
     data = request.json
     email = data.get('email')
     instagram = data.get('instagram')
-    API_OCTOPUS = os.environ.get('API_OCTOPUS')
+    #API_OCTOPUS = os.environ.get('API_OCTOPUS')
     
     if not email:
         return jsonify({'error': 'Email and Instagram are required'}), 400
@@ -21,7 +21,7 @@ def subscribe():
     }
 
     data = {
-    "api_key": API_OCTOPUS,
+    "api_key": "14b7bb29-58d0-4bd2-a66a-3bc874fd04fb",
     "email_address": email,
     "fields": {
         "FirstName": instagram
