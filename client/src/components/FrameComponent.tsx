90ca7f51bc10394d86579b51aa13a150bbd72cfb
Daniel Spiri <danielspiridonovpoch@gmail.com>
2024-06-14 17:35:17 +0200

    cambio de ruta backend

--- client/src/components/FrameComponent.tsx
+++ client/src/components/FrameComponent.tsx
@@ -25,7 +25,7 @@ const FrameComponent = forwardRef<HTMLFormElement, FrameComponentType>(({
     });
 
     try {
-      const response = await fetch(`https://web3-eight-mocha.vercel.app/subscribe`, {  // URL de tu servidor Flask
+      const response = await fetch(`https://flask-backend-self.vercel.app/subscribe`, {  // URL de tu servidor Flask
         method: 'POST',
         headers: headers,
         body: data
